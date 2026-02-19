9f86307bd2a95d8bdefb47666d7711207663541d
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-19 14:27:42 +0530

    update

--- index5.js
+++ index5.js
@@ -4,9 +4,18 @@ app.listen(8080,()=>
     {
         console.log("Server started")
     })
-app.get("/",(req,res)=>
+// app.get("/",(req,res)=>
+//     {
+//         //console.log(req.headers)
+//         console.log(req.headers.authorization)
+//         res.send("Hello World")
+//     })    
+
+//we can write anything as arguments here instead of req and res
+app.get("/",(a,b)=>
     {
-        console.log(req.headers)
-        console.log(req.headers)
-        res.send("Hello World")
-    })    
+        //console.log(req.headers)
+        console.log(a.headers.authorization)
+        b.send("Hello World")
+    })  
+//req is the variable name through which we pass the data to web server and access later
