6e23b4fe208e7b2dd432a14c843f1461b6b50c34
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-19 13:48:34 +0530

    update

--- index4a.js
+++ index4a.js
@@ -10,5 +10,12 @@ app.get("/:num1/:num2", (req, res) => {
     const a = Number(req.params.num1)
     const b = Number(req.params.num2)
 
-    res.send(String(a + b))
+    res.send(a + b)
+})
+app.get("/x/:num1/y/:num2/z/:num3", (req, res) => {
+    //http://localhost:8080/x/4/y/5/z/1
+    const a = Number(req.params.num1)
+    const b = Number(req.params.num2)
+    const c = Number(req.params.num3)
+    res.send(a + b + c)
 })
