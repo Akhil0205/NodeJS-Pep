1e04b1ef4df1a57402409eb14c3e0d740cc835bd
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-19 11:40:57 +0530

    update

--- index3.js
+++ index3.js
@@ -6,13 +6,16 @@ app.listen(8080,()=>
     });
 app.get("/",(req,res)=>
     {
+        console.log(req.url)//prints the url in terminal
         res.send("resonse from server for /")
     })    
 app.get("/home",(req,res)=>
     {
+        console.log(req.url)
         res.send("resonse from /home")
     })    
 app.get("/home/page1",(req,res)=>
     {
+        console.log(req.url)
         res.send("resonse from /home/page1")
     })        
