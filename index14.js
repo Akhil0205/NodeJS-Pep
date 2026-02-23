0135eab34c27a775e74bacb93c670b2c0837c809
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-23 13:36:27 +0530

    update

--- index14.js
+++ index14.js
@@ -1,5 +1,15 @@
 //json web token
+//we want to create a string from an input
 import express from 'express'
-import JsonWebTokenError from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
+const SECRET='lpu'
 const app=express()
 app.listen(8080,()=>console.log("Server started"))
+const user=
+{
+    name:"John",
+    email:"j@gmail.com",
+    role:"student"
+}
+const token=jwt.sign(user,SECRET,{expiresIn:"1h"})
+console.log(token)
