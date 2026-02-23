e37b72c470aa6ddeb802da74ee6444255a628132
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-23 09:37:37 +0530

    update

--- index10.js
+++ index10.js
@@ -1,9 +1,11 @@
+//sending status code 
 import express from 'express'
 const app=express()
 app.get("/",(req,res)=>
     {
-        res.json({message:"Hello world"})
-       
+        // res.json({message:"Hello world"})
+        res.status(201).json({message:"Hello world"})
+        //sending status as well manually
     })
 app.listen(8080,()=>
     {
