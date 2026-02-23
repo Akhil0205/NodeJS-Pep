2af62f08762aaa994ee181d549aa0636898b61bc
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-23 11:47:21 +0530

    update

--- index13.js
+++ index13.js
@@ -1,8 +1,15 @@
 import express from 'express'
 const app=express()
 app.listen(8080,()=>console.log("Server started"))
-const users=[]
+const users=[] 
+app.use(express.json())
 app.post("/signup",(req,res)=>
     {
-        
+        const body=req.body;
+        users.push(body)
+        res.json(users)
     })
+app.get("/users",(req,res)=>
+    {
+        res.json(users)
+    })    
